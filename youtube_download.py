712a5d3a82db7ace847216ea3185ff6288d6b84c
daicha-w <58203483+daicha-w@users.noreply.github.com>
2023-12-11 02:57:05 +0900

    set output directory

--- youtube_download.py
+++ youtube_download.py
@@ -1,5 +1,5 @@
 from yt_dlp import YoutubeDL
 
-option = {'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]"}
+option = {'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]", 'outtmpl': "./out/%(title)s.%(ext)s"}
 ydl = YoutubeDL(option)
 ydl.download(input("Enter the URL: "))
