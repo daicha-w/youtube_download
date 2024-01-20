b179dc92a6530046b5fbb1ad97d0423458ad6668
daicha-w <58203483+daicha-w@users.noreply.github.com>
2024-01-21 02:46:05 +0900

    Add option to choose between video and audio downloads

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from yt_dlp import YoutubeDL
 
-option = {'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]", 'outtmpl': "./out/%(title)s.%(ext)s"}
+option = {}
+
+print("1: Video, 2: Audio")
+match int(input("Enter the option: ")):
+    case 1:
+        option = {
+            "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]",
+            "outtmpl": "./out/%(title)s.%(ext)s",
+        }
+    case 2:
+        option = {"format": "bestaudio[ext=m4a]", "outtmpl": "./out/%(title)s.%(ext)s"}
+    case _:
+        print("Error: Invalid option")
+        exit()
 ydl = YoutubeDL(option)
 ydl.download(input("Enter the URL: "))
