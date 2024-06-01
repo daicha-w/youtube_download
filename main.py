16e6d2d5084cce572f80f3e8ae2e727ee44e445e
daicha-w <58203483+daicha-w@users.noreply.github.com>
2024-06-01 13:52:10 +0900

    ライブ配信

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from yt_dlp import YoutubeDL
 
 option = {}
 
-print("1: Video, 2: Audio")
+print("0: Live, 1: Video, 2: Audio")
 match int(input("Enter the option: ")):
-    case 1:
+    case 0:
         option = {
             "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]",
             "outtmpl": "./out/%(title)s.%(ext)s",
+            "live_from_start": "True"
+        }
+    case 1:
+        option = {
+            "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]",
+            "outtmpl": "./out/%(title)s.%(ext)s"
         }
     case 2:
         option = {"format": "bestaudio[ext=m4a]", "outtmpl": "./out/%(title)s.%(ext)s"}
